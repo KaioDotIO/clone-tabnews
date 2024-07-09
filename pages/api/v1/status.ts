1a70a2e84cef7e358e2d0db4d882d9ab69a2cccc
Kaio César Ribeiro <93557244+KaioDotIO@users.noreply.github.com>
2024-07-09 23:02:39 +0000

    database.ts and .env

--- pages/api/v1/status.ts
+++ pages/api/v1/status.ts
@@ -1,4 +1,8 @@
-function status(request: any, response: any) {
+import database from "../../../infra/database";
+
+async function status(request: any, response: any) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   response.status(200).json({
     chave: "Valor",
   });
